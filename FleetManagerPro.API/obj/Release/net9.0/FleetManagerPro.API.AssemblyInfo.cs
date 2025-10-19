826598392bda2a0e1b09e973df114f8128076941
ThatOneJames <deocampojamesfrancis31@gmail.com>
2025-10-19 17:15:48 +0800

    Trial for Railway

--- FleetManagerPro.API/obj/Release/net9.0/FleetManagerPro.API.AssemblyInfo.cs
+++ FleetManagerPro.API/obj/Release/net9.0/FleetManagerPro.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FleetManagerPro.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb27bf682340d01ed3a50e7b5576a07385a59e05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+031c98f608f6594d1402cd84bf416a82980d8553")]
 [assembly: System.Reflection.AssemblyProductAttribute("FleetManagerPro.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FleetManagerPro.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
